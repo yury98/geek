3d682282cae6a21d172ec9979a8d011bb83029a4
Yury Shashkin <yshashkin@yandex.ru>
2018-06-08 20:06:08 +0300

    Revert "Revert "Revert "12"""
    This reverts commit e72cdcad5d859e4bf0b65a4a31eac82721113540.

--- task2.rb
+++ task2.rb
@@ -1,7 +1,7 @@
 a = ARGV[0].to_s
 b = ARGV[1].to_s
 i=0
-if (a.length >=1) && (b.length<=10000) && (b.length>0)
+if (a.length >=1) && (b.length<=10000)
   a1=a
   a2 =a
   a.length.times do
@@ -18,6 +18,6 @@ if (a.length >=1) && (b.length<=10000) && (b.length>0)
   end
   d=i
   puts c if c<d
-  puts d if d<c
+  puts d if (d<=c) && (d!=a.length)
   puts -1 if (d==a.length) && (c==a.length)
 end
